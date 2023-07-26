dd8799e17b55d7746d8d32ffdbb51953f5c0b927
alxmart <alxmartinez@gmail.com>
2023-07-26 15:18:06 -0300

    Defined Intent / opening DetalhesActivity.

--- app/src/main/java/com/luizafmartinez/combustivel/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/combustivel/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.luizafmartinez.combustivel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnCalcular : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+            val intent = Intent (this, DetalhesActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
