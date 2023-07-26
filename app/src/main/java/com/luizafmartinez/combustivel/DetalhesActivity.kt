b37a10772153d1d2bbd08d7bacb6f44c423294fa
alxmart <alxmartinez@gmail.com>
2023-07-26 17:22:39 -0300

    Added code to putExtra /calculation / show result.

--- app/src/main/java/com/luizafmartinez/combustivel/DetalhesActivity.kt
+++ app/src/main/java/com/luizafmartinez/combustivel/DetalhesActivity.kt
@@ -3,13 +3,43 @@ package com.luizafmartinez.combustivel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class DetalhesActivity : AppCompatActivity() {
 
-    lateinit var btnFechar : Button
+    private lateinit var btnFechar : Button
+
+    private lateinit var textGasolina : TextView
+    private lateinit var textAlcool : TextView
+    private lateinit var textResultado : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
+
+       textGasolina = findViewById(R.id.txt_gasolina)
+       textAlcool = findViewById(R.id.txt_alcool)
+       textResultado = findViewById(R.id.txt_resultado)
+
+       val bundle = intent.extras
+
+       if (bundle != null) {
+
+           val gasolina = bundle.getDouble("gasolina")
+           val alcool = bundle.getDouble("alcool")
+
+           textGasolina.text = "Preço Gasolina: $gasolina"
+           textAlcool.text = "Preço Álcool: $alcool"
+
+           val resultado = (alcool / gasolina)
+
+           if (resultado > 0.7) {
+               textResultado.text = "Gasolina"
+           } else {
+               textResultado.text = "Álcool"
+           }
+
+       }
 
         btnFechar = findViewById(R.id.btn_fechar)
         btnFechar.setOnClickListener {
--- app/src/main/java/com/luizafmartinez/combustivel/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/combustivel/MainActivity.kt
@@ -4,18 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnCalcular : Button
+    lateinit var editPrecoGas : EditText
+    lateinit var editPrecoAlc : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        editPrecoGas = findViewById(R.id.preco_gas)
+        editPrecoAlc = findViewById(R.id.preco_alc)
         btnCalcular = findViewById(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
+
             val intent = Intent (this, DetalhesActivity::class.java)
+
+            val gasolina = editPrecoGas.text.toString()
+            val alcool   = editPrecoAlc.text.toString()
+
+            if ( gasolina.isNotEmpty() && alcool.isNotEmpty() ) {
+                intent.putExtra( "gasolina" , gasolina.toDouble())
+                intent.putExtra( "alcool" , alcool.toDouble())
+            }
+
             startActivity(intent)
         }
 
