bf576496c713ca8915cf9d42be1aee06b2d3eccf
alxmart <alxmartinez@gmail.com>
2023-07-26 16:11:48 -0300

    Added finish() method to close activity.

--- app/src/main/java/com/luizafmartinez/combustivel/DetalhesActivity.kt
+++ app/src/main/java/com/luizafmartinez/combustivel/DetalhesActivity.kt
@@ -2,10 +2,19 @@ package com.luizafmartinez.combustivel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetalhesActivity : AppCompatActivity() {
+
+    lateinit var btnFechar : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
+
+        btnFechar = findViewById(R.id.btn_fechar)
+        btnFechar.setOnClickListener {
+            finish()
+        }
+
     }
 }
